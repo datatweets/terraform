cf4803e1bd3d55d255afddcb04c0b3a16379dc41
Mehdi Lotfinejad <lotfinejad@gmail.com>
2024-11-08 23:43:24 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,14 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = var.aws_region
 }
